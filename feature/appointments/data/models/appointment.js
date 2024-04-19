2568a6018e37d10af15fb42a4bc2ce01e96f0217
Marko Petrovski <markopetrovskimp@gmail.com>
2024-04-19 23:17:21 +0200

    Add conclusion and handledBy to Appointment

--- feature/appointments/data/models/appointment.js
+++ feature/appointments/data/models/appointment.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose')
 
 const appointmentSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    date: { type: Date, required: true },
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  handledBy: { type: String, required: true },
+  conclusion: { type: String, required: false }
 },
-    {
-        toJSON: {
-            transform: function(doc, ret) {
-                ret.id = ret._id;
-                delete ret._id;
-                delete ret.__v
-            }
-        }
-    })
+{
+  toJSON: {
+    transform: function (doc, ret) {
+      ret.id = ret._id
+      delete ret._id
+      delete ret.__v
+    }
+  }
+})
 
 module.exports = mongoose.model('Appointment', appointmentSchema)
